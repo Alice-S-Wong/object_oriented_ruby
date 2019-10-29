8cdb99505fceb659ae219bf58bb9f709d31ce733
“Alice <alicexenawong@gmail.com>
2019-10-28 17:56:10 -0700

    Add a line to print info about item 1

--- store_item.rb
+++ store_item.rb
@@ -1,3 +1,5 @@
 item1 = {:name => "pear", :price => 2, :color => "green"}
 item2 = {:name => "apple", :price => 1, :color => "red"}
 item3 = {:name => "banana", :price => 3, :color => "yellow"}
+
+p "#{item1[:name]} is $#{item1[:price]} per item. They are #{item1[:color]} in color."
